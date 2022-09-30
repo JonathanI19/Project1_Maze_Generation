bf1bfb04d3433078e03d091eaa5a04dfcaa06b2f
JonathanI19 <jmisely19@gmail.com>
2022-09-29 22:01:14 -0400

    csv write issue

--- example/example.py
+++ example/example.py
@@ -7,9 +7,9 @@ from pyamaze import maze
 OUTPUT="./example_output.csv"
 INPUT="./input.txt"
 def main():
-    #parse = ParseFile(input_file=INPUT, output_file=OUTPUT)
-    #m = maze(rows=parse.rows, cols=parse.cols)
-    m = maze()
+    parse = ParseFile(input_file=INPUT, output_file=OUTPUT)
+    m = maze(rows=parse.rows, cols=parse.cols)
+    #m = maze()
     m.CreateMaze(loadMaze='./dfs.csv')
     m.run()
 
