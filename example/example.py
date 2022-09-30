bbd6bf6f7df0780c750bf7d9781e35a98f3d08e7
JonathanI19 <jmisely19@gmail.com>
2022-09-29 21:47:33 -0400

    fixed??

--- example/example.py
+++ example/example.py
@@ -5,11 +5,12 @@ from parser_script import ParseFile
 from pyamaze import maze
 
 OUTPUT="./example_output.csv"
-INPUT="./input2.txt"
+INPUT="./input.txt"
 def main():
-    parse = ParseFile(input_file=INPUT, output_file=OUTPUT)
-    m = maze(rows=parse.rows, cols=parse.cols)
-    m.CreateMaze(loadMaze=OUTPUT)
+    #parse = ParseFile(input_file=INPUT, output_file=OUTPUT)
+    #m = maze(rows=parse.rows, cols=parse.cols)
+    m = maze()
+    m.CreateMaze(loadMaze='./dfs.csv')
     m.run()
 
 if __name__=="__main__":
